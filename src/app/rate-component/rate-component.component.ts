4dfe5c869ca8aa21020076c2d4565b76c3b4f0a1
ahmed-hefny <ahmedhgad@gmail.com>
2021-04-12 18:51:54 +0200

    Created Rate component with New Branch

---
+++ src/app/rate-component/rate-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rate-component',
+  templateUrl: './rate-component.component.html',
+  styleUrls: ['./rate-component.component.css']
+})
+export class RateComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
