2f5ea4b2ad6dc9020952e63631c55a9d6f05263b
Shehab <shehabshetos83@gmail.com>
2021-04-12 22:58:39 +0200

    initializing reviewCard

---
+++ src/app/reviewCard/reviewCard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reviewCard',
+  templateUrl: './reviewCard.component.html',
+  styleUrls: ['./reviewCard.component.css']
+})
+export class ReviewCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
