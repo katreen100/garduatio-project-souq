58bdc0efd46fe17018b4c97eeb605e8ac5e77556
ahmed-hefny <ahmedhgad@gmail.com>
2021-04-13 18:40:50 +0200

    adding Rate component

--- src/app/Components/rate-component/avg/rate-component.component.ts
+++ src/app/Components/rate-component/avg/rate-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rate-component',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rate-component.component.css']
 })
 export class RateComponentComponent implements OnInit {
+  @Input() currentRate: number;
+  @Input() size:string;
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit(): void {
   }
