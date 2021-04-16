942100cb47040e280501d5792f8aaead1198a4c4
Mostafa ElGuindi <mielguindi@gmail.com>
2021-04-16 22:55:21 +0100

    fix package-lock typo and NgbModule duplicate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import{OneProductCompComponent} from './productComp/oneProductComp/oneProductComp.component';
 import {FreeShipppingmodalComponent } from './lay out/freeShipppingmodal/freeShipppingmodal.component'
 import {ProductgallaryComponent} from "./lay out/productgallary/productgallary.component"
-
-
-
 import { RateComponentComponent } from './Components/rate-component/avg/rate-component.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RateStaticsComponent } from './Components/rate-component/rate-statics/rate-statics.component';
@@ -26,32 +20,23 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-
     OneProductCompComponent,
     FreeShipppingmodalComponent,
     ProductgallaryComponent,
-  
-
     RateComponentComponent,
     RateStaticsComponent,
     UserRateComponent,
     ProductRateComponent,
     FilterationComponent
-
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     AppRoutingModule,
-
-    NgbModule
-
     NgbModule,
     NgxSliderModule,
     FormsModule
-    
-
   ],
   providers: [],
   bootstrap: [AppComponent]
