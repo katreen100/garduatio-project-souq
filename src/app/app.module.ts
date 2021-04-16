59639c703cef8fa4421d81c5b9acbccf7245419c
Mostafa ElGuindi <mielguindi@gmail.com>
2021-04-16 23:41:29 +0100

    fixing typo in appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import { FormsModule } from '@angular/forms';
     ProductRateComponent,
     ReviewCardComponent,
     ProductInfoComponent,
-    ReviewsListComponent
+    ReviewsListComponent,
     FilterationComponent
   ],
   imports: [
