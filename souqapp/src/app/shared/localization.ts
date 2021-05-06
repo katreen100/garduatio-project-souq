b8ccad9356e4a2c621ec350fe73e1b07cada9b95
Mostafa ElGuindi <mielguindi@gmail.com>
2021-05-06 20:06:01 +0100

    added a shared module with necessary path shortcut in ts.config

---
+++ souqapp/src/app/shared/localization.ts
@@ -0,0 +1,8 @@
+let locale = 'ar-EG';
+
+
+// table names
+let category = (locale == 'ar-EG') ? 'categoryArabic' : 'categoryEnglish';
+let product = (locale == 'ar-EG') ? 'productArabic' : 'productEnglish';
+
+export { category, product };
