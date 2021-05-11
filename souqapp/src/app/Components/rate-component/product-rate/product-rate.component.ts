9e14b8d3bace91f8498fc390c5df16c30c540c35
ahmed-hefny <ahmedhgad@gmail.com>
2021-05-11 22:24:23 +0200

    added new service of Data Model

--- souqapp/src/app/Components/rate-component/product-rate/product-rate.component.ts
+++ souqapp/src/app/Components/rate-component/product-rate/product-rate.component.ts
@@ -1,29 +1,48 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IRate } from 'src/app/Viewmodels/Rate/irate';
+import { ProductService } from 'src/services/product.service';
 
 @Component({
   selector: 'app-product-rate',
   templateUrl: './product-rate.component.html',
   styleUrls: ['./product-rate.component.css']
 })
-export class ProductRateComponent implements OnInit {
-  @Input() productRate:IRate;
-  @Input() avgRate:number;
-  prdRateFlag:boolean;
-  constructor() {
+export class ProductRateComponent implements OnInit, OnDestroy {
+  productRate: IRate;
+  // @Input() avgRate: number;
+  @Input() parentProduct: string;
+  private subscriptions: Subscription[] = []
+  prdRateFlag: boolean;
+  constructor(private rateService: ProductService) {
     this.productRate = {
-      oneStar: 2,
-      twoStar: 0,
-      threeStar: 0,
-      fourStar: 6,
-      fiveStar: 27,
-      total: 35
+      averageRating: 0,
+      one: 0,
+      two: 6,
+      three: 0,
+      four: 0,
+      five: 0,
     }
-    this.avgRate=3.6
-    this.prdRateFlag = this.avgRate==0? true:false;
-   }
+
+
+    this.parentProduct = 'ed0f0600-854e-4ad4-b01d-bda780b2cdc0'
+  }
+
 
   ngOnInit(): void {
+    let sub = this.rateService.getProductRatingDetails(this.parentProduct).subscribe(res => {
+      console.log(res)
+      // console.log(res.averageRating)
+      this.productRate = res;
+      this.prdRateFlag = this.productRate.averageRating == 0 ? true : false;
+    })
+    this.subscriptions.push(sub)
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => {
+      sub.unsubscribe();
+    })
   }
 
 }
