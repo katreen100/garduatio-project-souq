f9b7318da5f9fffc39206cb9fc021545148476fe
katreen100 <katyebraheem@gmail.com>
2021-05-11 21:57:15 +0200

    product with new service

--- souqapp/src/app/productComp/oneProductComp/oneProductComp.component.ts
+++ souqapp/src/app/productComp/oneProductComp/oneProductComp.component.ts
@@ -1,75 +1,50 @@
-import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { NgbDropdown} from '@ng-bootstrap/ng-bootstrap'
+import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
 
-import { ProductService } from 'src/old-services/product.service';
-
+import { ProductService } from 'src/services/product.service';
 
 @Component({
   selector: 'app-oneProductComp',
   templateUrl: './oneProductComp.component.html',
   styleUrls: ['./oneProductComp.component.css'],
-  providers: [NgbModalConfig, NgbModal,NgbDropdown]
+  providers: [NgbModalConfig, NgbModal, NgbDropdown],
 })
 export class OneProductCompComponent implements OnInit {
   product;
-  
 
-
-  constructor( config: NgbModalConfig, private modalService: NgbModal ,private prodservice:ProductService) { 
+  constructor(
+    config: NgbModalConfig,
+    private modalService: NgbModal,
+    private prodservice: ProductService
+  ) {
     config.backdrop = 'static';
     config.keyboard = false;
   }
-  // getprodvarityKeys(varities){
-  //       this.productvarityKey=Object.keys(varities)
-  //       this.productvarityvalue=Object.values(varities)
-  //       for(let i of this.productvarityvalue){
-  //         console.log(i)
-  //         this.productvarity2=i;
-          
-        
-  //       }
-       
-
-        
-  // }
   openlg(content) {
-    
     this.modalService.open(content, { size: 'lg' });
-  
   }
-  calPricefterDiscount(){
-    return this.product.price-(this.product.price*this.product.discount/100)
-
-   }
-  
-  calcSavedMony():number{
-    return this.product.price-this.calPricefterDiscount()
-   }
-  
-  
+  calPricefterDiscount() {
+    return (
+      this.product.price - (this.product.price * this.product.discount) / 100
+    );
+  }
+  calcSavedMony(): number {
+    return this.product.price - this.calPricefterDiscount();
+  }
 
   ngOnInit() {
-
-  this.prodservice.getProduct(1).subscribe(res=>{
-  
-
-console.log(res)
-  this.product=res[0]
-console.log(this.product)
-
-
-
-
-
-
-
-
-})
-
+    let parentid = ' ed0f0600-854e-4ad4-b01d-bda780b2cdc0';
+    this.prodservice
+      .getFullProduct(
+        'ed0f0600-854e-4ad4-b01d-bda780b2cdc0',
+        'Z9yl9x6K6ypb6Q18ow0R'
+      )
+      .subscribe((res) => {
+        console.log(res);
+        this.product = res[0];
+        console.log(this.product);
+      });
   }
 }
-
-
-
