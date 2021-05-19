138bbeede0455cbc771ad2b893bcd32905808ecf
Mostafa Saqly <63963889+mostafasaqly@users.noreply.github.com>
2021-05-19 12:19:36 +0200

    login page with rules admin -susriber - editor

--- adminapp/src/pages/auth/index.js
+++ adminapp/src/pages/auth/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {auth as fire} from './../../network/firebase/firebaseConfig';
+import { auth, db } from './../../network/firebase/firebaseConfig';
 import './../../App.css';
 import Login from './login/login';
 import Sucess from './success/success';
@@ -12,6 +12,23 @@ const LoginPage = () => {
     const [emailError, setEmailError] = useState('');
     const [passwordError, setPasswordError] = useState('');
 
+    const [userRole, setUserRole] = useState(false)
+    const [users, setUsers] = useState([])
+
+    const fetchUsers = () => {
+        db.collection("admins")
+            .get()
+            .then(querySnapshot => {
+                const data = querySnapshot.docs.map(doc => doc.data());
+                setUsers(data);
+                console.log(data);
+            });
+    }
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
+
     const clearInputs = () => {
         setEmail('');
         setPassword('');
@@ -22,33 +39,46 @@ const LoginPage = () => {
     }
     const handleLogin = () => {
         clearErrors();
-        fire
-            .signInWithEmailAndPassword(email, password)
-            .catch((err) => {
-                switch (err.code) {
-                    case "auth/Invalid-email":
-                    case "auth/user-disabled":
-                    case "auth/user-not-found":
-                        setEmailError(err.message);
-                        break;
-                    case "auth/wrong-password":
-                        setPasswordError(err.message);
-                        break;
-                    default:
-                        break;
-                }
-            });
+        users && users.forEach(user => {
+            if (user.email === email) {
+                console.log('done');
+                setUserRole(user.userRole);
+                console.log(userRole);
+                auth.signInWithEmailAndPassword(email, password)
+                    .catch((err) => {
+                        switch (err.code) {
+                            case "auth/Invalid-email":
+                            case "auth/user-disabled":
+                            case "auth/user-not-found":
+                                setEmailError(err.message);
+                                break;
+                            case "auth/wrong-password":
+                                setPasswordError(err.message);
+                                break;
+                            default:
+                                break;
+                        }
+                    });
+            }
+            else {
+                clearErrors();
+                setEmailError("Sorry, you are not admin")
+            }
+            
+        })
     };
 
     const handleLogOut = () => {
-        fire.signOut();
+        auth.signOut();
     }
 
     const authListener = () => {
-        fire.onAuthStateChanged((user) => {
+        auth.onAuthStateChanged((user) => {
             if (user) {
                 clearInputs();
                 setUser(user);
+                // console.log(user.uid);
+
             }
             else {
                 setUser('');
@@ -57,11 +87,11 @@ const LoginPage = () => {
     }
     useEffect(() => {
         authListener();
-    }, )
+    })
     return (
         <div className="App">
 
-            {user ? (<Sucess handleLogOut={handleLogOut} />) :
+            {user ? (<Sucess handleLogOut={handleLogOut} userRole={userRole}/>) :
 
                 (
                     <Login email={email}
@@ -73,7 +103,6 @@ const LoginPage = () => {
                         passwordError={passwordError}
                     />
                 )}
-
 
         </div>
     );
--- adminapp/src/pages/auth/success/success.js
+++ adminapp/src/pages/auth/success/success.js
@@ -1,16 +1,7 @@
-import React from 'react';
+import OrderSectionPage from '../../dashboard/order'
+const Sucess = ({ handleLogOut, userRole }) => {
 
-const Sucess = ({ handleLogOut }) => {
-    const handlAdmin=()=>{
-        console.log("Create and Delete and Update and Read");
-    }
-    const handleEditor =()=>{
-        console.log("Read and Update");
-    }
-    const handleSubscriber = ()=>{
-        console.log("read");
-    }
-    
+
     return <section>
         <div style={{ height: 100 }}> </div>
         <div>
@@ -18,11 +9,13 @@ const Sucess = ({ handleLogOut }) => {
         </div>
         <div style={{ height: 100 }}> </div>
         <div>
-            <button onClick={handlAdmin}>Admin</button>
-            <button onClick={handleSubscriber}>Subscriber</button>
-            <button onClick={handleEditor}>Editor</button>
+            <h3> {userRole} </h3>
+            <OrderSectionPage userRole={userRole} />
+
         </div>
-        
+
+
+
     </section>
 }
 export default Sucess;
