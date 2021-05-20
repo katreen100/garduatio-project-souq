624e1e912799d965bfceda5a3bf1f73f75be0bef
Mostafa Saqly <63963889+mostafasaqly@users.noreply.github.com>
2021-05-20 02:15:39 +0200

    add roles

--- adminapp/src/App.js
+++ adminapp/src/App.js
@@ -1,22 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DashboardPage from './pages/dashboard/dashboard';
-import {db} from './network/firebase/firebaseConfig';
+//import DashboardPage from './pages/dashboard/dashboard';
+import OrderSectionPage from './pages/dashboard/order';
 import './App.css';
 
 
-function App() {
-  db.collection('brands')
-          .get()
-          .then(response => {
-            return response.docs.map(doc => {
-              console.log(doc.data());
-              return doc.data();
-            })
-          })
-          .catch(error => console.log(error))
+function App({userRole, handleLogOut}) {
+
   return (
     <div className="App">
-      <DashboardPage />
+      {/* <DashboardPage userRole={userRole} handleLogOut={handleLogOut}/> */}
+      <OrderSectionPage userRole={userRole} handleLogOut={handleLogOut}/>
     </div>
   );
 }
