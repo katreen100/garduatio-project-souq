37c16dd7253c436926c40060f58ac2372348c791
Mostafa ElGuindi <mielguindi@gmail.com>
2021-06-16 07:44:23 +0200

    successful deployment commit

--- souqapp/src/environments/environment.prod.ts
+++ souqapp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyChjdJkToFESdr0paKRt2wUkKVwHJiQc4Y",
+    authDomain: "ecommerce-clone-site.firebaseapp.com",
+    projectId: "ecommerce-clone-site",
+    storageBucket: "ecommerce-clone-site.appspot.com",
+    messagingSenderId: "539446440742",
+    appId: "1:539446440742:web:e59ae9d8f0661afcc647e7"
+  }
 };
